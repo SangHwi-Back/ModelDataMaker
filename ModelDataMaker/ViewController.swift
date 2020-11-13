bf9a00053330dced9b610f367e39e00a7bde4754
Sanghwi Back <sanghwiback@gmail.com>
2020-11-13 21:52:26 +0900

    Initial Commit

---
+++ ModelDataMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModelDataMaker
+//
+//  Created by 백상휘 on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
